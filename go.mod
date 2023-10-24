6027817c83450e498935e48d3e6dbc00b5d81ba6
Rushikesh Naik <rushikeshnaik@Rushikeshs-MBP.MG8702>
2023-10-24 15:13:54 -0700

    Added Ring Queue sample code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golearning
 
 go 1.21.0
+
+require github.com/aws/aws-lambda-go v1.41.0
--- routines.go
+++ routines.go
@@ -5,32 +5,37 @@ import (
 	"sync"
 )
 
-var ch chan int
-
-func processMessage(task *sync.WaitGroup) {
+func processMessage(threadName string, task *sync.WaitGroup, ch <-chan int) {
 	defer task.Done()
-	v := <-ch
-
-	fmt.Println("Print value=", v)
+	for {
+		v, ok := <-ch
+		if ok {
+			fmt.Println("Print value in thread (", threadName, "), v=", v)
+		} else {
+			fmt.Println("terminating go routing=", threadName)
+			break
+		}
+	}
 }
 
-func main10() {
+func main() {
+	var ch chan int
 
 	ch = make(chan int, 10)
 
-	var arry []int = []int{1, 5, 6, 2, 8, 9, 10}
+	var arry []int = []int{1, 5, 6, 2, 3, 4, 8, 9, 7, 10}
 
 	for _, v := range arry {
 		ch <- v
 	}
+	close(ch)
 
 	var wg sync.WaitGroup
 
-	index := 0
-	for index < 7 {
-		wg.Add(1)
-		go processMessage(&wg)
-	}
+	wg.Add(3)
+	go processMessage("First", &wg, ch)
+	go processMessage("Second", &wg, ch)
+	go processMessage("Third", &wg, ch)
 	wg.Wait()
 
 }
